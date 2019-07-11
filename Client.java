b88fe46a94b37947bf3af1416136a6aae7bec6b1
Moiiwa <52782663+Moiiwa@users.noreply.github.com>
2019-07-11 18:10:13 +0500

    Update Client.java
    Added comments for client

--- Client.java
+++ Client.java
@@ -16,20 +16,16 @@ public class Client {
             byte adr[]=new byte[]{(byte)192,(byte)168,0,(byte)191};         //put your adress instead
             socket = new Socket(InetAddress.getByAddress(adr),1337);        //and port, if needed
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            chatWindow=new ChatWindow();
-            chatWindow.setVisible(true);
-            chatWindow.readArea.append(in.readLine()+"\n");
-            new ClientFunctions(in).start();
-            while (!message.equalsIgnoreCase("Stop")){
-                message=chatWindow.writeArea.getText();
-                //out.write(message+"\n");
-                //out.flush();
+            chatWindow=new ChatWindow();                                    //creation of gui
+            chatWindow.setVisible(true);                                    //making it visible
+            chatWindow.readArea.append(in.readLine()+"\n");                 //write to readArea
+            new ClientFunctions(in).start();                                //start new thread
+            while (!message.equalsIgnoreCase("Stop")){                      //while keyword "Stop" was not sent
+                message=chatWindow.writeArea.getText();                     //read text from writeArea
             }
             in.close();
             out.close();
-            console.close();
             return;
         } catch (IOException e) {
             e.printStackTrace();
@@ -47,40 +43,40 @@ class ClientFunctions extends Thread{
         try {
 
             while (!Client.message.equalsIgnoreCase("stop"))
-                Client.chatWindow.readArea.append(reader.readLine() + "\n");
+                Client.chatWindow.readArea.append(reader.readLine() + "\n");    //write message sent from server to readArea
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
 
-class ChatWindow extends JFrame{
-    public TextArea readArea = new TextArea(9,64);
-    public TextField writeArea = new TextField(64);
+class ChatWindow extends JFrame{                                //description of gui
+    public TextArea readArea = new TextArea(9,64);              //readArea consists of 9 rows and 64 columns
+    public TextField writeArea = new TextField(64);             //textArea of only 1 row and 64 columns
     public ChatWindow(){
-        super("Moiwa-chat");
-        this.setBounds(500,500,500,250);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super("Moiwa-chat");                                    //title of the window
+        this.setBounds(500,500,500,250);                        //description of size and coordinates of window
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
         this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e){
                 try {
-                    Client.out.write("stop\n");
+                    Client.out.write("stop\n");                 //when window is closed client send keyword to stop chatting
                     Client.out.flush();
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
             }
         });
-        readArea.setEditable(false);
-        Container container=this.getContentPane();
-        container.setLayout(new FlowLayout());
-        container.add(readArea);
+        readArea.setEditable(false);                           //we make readArea non-editable, to make users write only in writeArea
+        Container container=this.getContentPane();      
+        container.setLayout(new FlowLayout());                 //flowlayout is used because it looks more flexible than other layouts
+        container.add(readArea);                               //add readArea and writeArea
         container.add(writeArea);
         writeArea.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    Client.out.write(Client.message+"\n");
+                    Client.out.write(Client.message+"\n");      //when "Enter" button was pressed, send message to server and clean writeArea
                     Client.out.flush();
                     Client.chatWindow.writeArea.setText("");
                 } catch (IOException ex) {
